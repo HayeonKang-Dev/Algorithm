5df0984574079198f0355baaefed3b9b02dea11c
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2024-03-14 10:51:44 +0900

    Add files via upload

---
+++ multiple_sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main_1(void)
+{
+	int n, m, sum = 0;
+
+	scanf_s("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; i++)
+	{
+		if(i % m == 0)
+		{
+			sum += i;
+		}
+
+	}
+	printf("%d\n", sum);
+	return 0;
+}
