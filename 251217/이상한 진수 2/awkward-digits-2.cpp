74a83ad36d7eccb7d7d82c7a72dc1feaea01cbc5
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-17 16:09:55 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251217/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s; 
+int n; 
+int dig[11]; 
+int f() {
+    int tmp=0, d=0; 
+    for(int i=n-1; i>=0; i--) {
+        tmp += dig[i] * pow(2, d); 
+        d++; 
+    }
+    return tmp; 
+}
+
+int main() {
+    // Please write your code here.
+    cin>>s; 
+    n = s.size(); 
+    for(int i=0; i<n; i++) {
+        dig[i] = s[i]-'0'; 
+    }
+    int ans=0; 
+    for(int i=0; i<n; i++) {
+        if (dig[i] == 0) {
+            dig[i] = 1; 
+            ans = max(ans, f()); 
+            dig[i] = 0; 
+        }
+    }
+    cout<<ans; 
+    return 0;
+}
