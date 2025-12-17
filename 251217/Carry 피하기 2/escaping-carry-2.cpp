96dd1141647266521df2193f9e5d6b1041799efc
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-17 16:54:53 +0900

    Title: Carry 피하기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251217/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n; 
+vector<int> v; 
+bool f(int a, int b, int c) {
+    bool flag = true; 
+    int digit = 10; 
+    while(a > 0 && b > 0 && c >0 ) {
+        if (a%10 + b%10 + c%10 >= 10) return false; 
+        a/=10; b/=10; c/=10; 
+    }
+    return true; 
+}
+int main() {
+    // Please write your code here.
+    cin>>n; 
+    for(int i=0; i<n; i++) {
+        int a; 
+        cin>>a; 
+        v.push_back(a); 
+    }
+    int ans = -1; 
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            for(int k=j+1; k<n; k++) {
+                if (f(v[i], v[j], v[k])) {
+                    ans = max(ans, v[i]+v[j]+v[k]); 
+                }
+            }
+        }
+    }
+    cout<<ans; 
+    return 0;
+}
