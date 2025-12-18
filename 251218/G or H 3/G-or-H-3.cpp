cda6b4eee5bfc445fa3ef23b4c5cda82aff6b0f6
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-18 13:52:24 +0900

    Title: G or H 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251218/G or H 3/G-or-H-3.cpp
+++ 251218/G or H 3/G-or-H-3.cpp
@@ -5,25 +5,31 @@ int num[10002];
 int main() {
     // Please write your code here.
     cin>>n>>k; 
-    int mx = -1;
+    int mx = -1, mn=INT_MAX; 
     for(int i=0; i<n; i++) {
         char c; 
         int a; 
         cin>>a>>c; 
         mx = max(mx, a); 
+        mn = min(mn, a); 
         if (c=='G') {
             num[a] = 1; 
         }
         else num[a] = 2; 
     }
-    // for(int i=1; i<=mx; i++) cout<<num[i]<<' '; 
-    // cout<<'\n';
+    if (mn == mx) {
+        cout<<num[mn]; 
+        return 0;
+    }
     int ans = -1; 
-    for(int i=1; i<=mx-k; i++) {
+    for(int i=1; i<=10000; i++) {
         int tmp=0; 
+        // cout<<"I: "<<i<<", j: "<<i+k+1;
+        if (i+k+1 > 10000) break; 
         for(int j=i; j<i+k+1; j++) {
             tmp += num[j]; 
         }
+        // cout<<" => sum: "<<tmp<<'\n';
         ans = max(ans, tmp); 
     }
     cout<<ans; 
