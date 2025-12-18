fcab6eced8a16dfc6f4d84026f43be459e113b8c
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-18 15:10:54 +0900

    Title: 바구니 안의 사탕 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251218/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, candy[102]; 
+int main() {
+    // Please write your code here.
+    cin>>n>>k; 
+    for(int i=0; i<n; i++) {
+        int a, b; 
+        cin>>a>>b; 
+        candy[b] += a; 
+    }
+    int ans=0; 
+    for(int i=k; i<=n; i++) {
+        int tmp=0; 
+        // cout<<"범위: "<<i-k<<" ~ "<<i+k-1<<'\n';
+        // cout<<"왼쪽("<<i-k<<"), 중앙("<<i<<"), 오른쪽("<<i+k<<")\n"; 
+        for(int j=i-k; j<=i+k; j++) {
+            tmp += candy[j]; 
+        }
+        ans = max(ans, tmp); 
+    } 
+    cout<<ans; 
+    return 0;
+}
