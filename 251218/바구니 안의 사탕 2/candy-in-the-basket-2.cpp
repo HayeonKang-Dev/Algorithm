01f193352a18ae58af6632c67c7c48f6732b17cd
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-18 15:17:16 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251218/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 251218/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -10,12 +10,12 @@ int main() {
         candy[b] += a; 
     }
     int ans=0; 
-    for(int i=k; i<=100; i++) {
+    for(int i=0; i<=100; i++) {
         int tmp=0; 
         // cout<<"범위: "<<i-k<<" ~ "<<i+k-1<<'\n';
         // cout<<"왼쪽("<<i-k<<"), 중앙("<<i<<"), 오른쪽("<<i+k<<")\n"; 
-        int lim = min(100, i+k); 
-        for(int j=i-k; j<=lim; j++) {
+        int lim = min(100, i+2*k); 
+        for(int j=i; j<=lim; j++) {
             tmp += candy[j]; 
         }
         ans = max(ans, tmp); 
