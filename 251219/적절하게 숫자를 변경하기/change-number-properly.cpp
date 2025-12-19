305f02751160353f4f5e49d4c801a5c64792fc19
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-19 14:56:30 +0900

    Title: 적절하게 숫자를 변경하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251219/적절하게 숫자를 변경하기/change-number-properly.cpp
+++ 251219/적절하게 숫자를 변경하기/change-number-properly.cpp
@@ -1,48 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m; 
-int num[502], dp[502][5][102];
+int a[501]; 
+int dp[501][101][5]; 
 int main() {
     // Please write your code here.
     cin>>n>>m; 
+    for(int i=0; i<n; i++) cin>>a[i]; 
     for(int i=0; i<n; i++) {
-        cin>>num[i]; 
-    }
-    for(int i=0; i<n; i++) {
-        for(int j=1; j<=4; j++) {
-            for(int k=0; k<=m; k++) {
-                dp[i][j][k] = INT_MIN; 
-            }
+        for(int j=0; j<=m; j++) {
+            for(int k=1; k<=4; k++) dp[i][j][k] = INT_MIN; 
         }
     }
-    for(int i=1; i<=4; i++) {
-        if (i== num[0]) dp[0][i][0] = 1; 
-        else dp[0][i][0] = 0; 
-    }
+    for(int i=1; i<=m; i++) dp[0][0][i] = (i == a[0]); 
 
-    for(int i=1; i<n; i++) {
-        for(int j=1; j<=4; j++) {
-            for(int k=0; k<=m; k++) {
-                if (dp[i-1][j][k] == INT_MIN) continue; 
-
-                for(int c=1; c<=4; c++) {
-                    int diff = (j != c); 
-                    int nk = k + diff; 
-                    if (nk > m) continue;
-
-                    int gain =0; 
-                    if (c == num[i]) gain = 1; 
-                    else gain =0; 
-
-                    dp[i][c][nk] = max(dp[i][c][nk], dp[i-1][j][k] + gain); 
+    for(int i=1; i<n; i++) { // i번째 숫자까지 고려했을 때 
+        for(int j=0; j<=m; j++) { // 그 동안 숫자를 j번 변경했고 
+            for(int k=1; k<=4; k++) { // 마지막으로 사용한 숫자가 k라 했을 때 
+                for(int l=1; l<=4; l++) { // i-1번째에 사용한 숫자 l 
+                    if (l==k) {
+                        dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][l] + (a[i] == k)); 
+                    }
+                    if (l != k && j > 0) {
+                        dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-1][l] + (a[i] == k)); 
+                    }
                 }
             }
         }
     }
-
     int ans=0; 
-    for(int i=1; i<=4; i++) {
-        for(int j=0; j<=m; j++) {
+    for(int i=0; i<=m; i++) {
+        for(int j=1; j<=4; j++) {
             ans = max(ans, dp[n-1][i][j]); 
         }
     }
