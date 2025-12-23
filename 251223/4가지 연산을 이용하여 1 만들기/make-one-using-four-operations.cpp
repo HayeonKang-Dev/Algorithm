2bfe97dfc3b04446d83f2c24226a2c13bbb9dce7
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2025-12-23 12:24:37 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 10ms, Memory: 4MB, Status: Passed - Codetree

--- 251223/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 251223/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -1,22 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n; 
+int n, visited[1000005]; 
 int main() {
     // Please write your code here.
     cin>>n; 
-    queue<pair<int, int>> q; 
-    q.push({n, 0}); 
+    memset(visited, -1, sizeof(visited)); 
+    queue<int> q; 
+    visited[n] = 0; 
+    q.push(n); 
     while(q.size()) {
-        auto [x, cnt] = q.front(); q.pop(); 
-        if (x == 1) {
-            cout<<cnt; 
-            break; 
+        int x = q.front(); q.pop(); 
+        if (x==1) break; 
+        if (x-1 >= 0 && visited[x-1] == -1) {
+            visited[x-1] = visited[x] + 1 ; 
+            q.push(x-1); 
         }
-        q.push({x-1, cnt+1}); 
-        q.push({x+1, cnt+1}); 
-        if (x%2==0) q.push({x/2, cnt+1}); 
-        if (x%3==0) q.push({x/3, cnt+1}); 
+        if (visited[x+1] == -1) {
+            visited[x+1] = visited[x] + 1; 
+            q.push(x+1); 
+        }
+        if (x%2==0 && visited[x/2] == -1) {
+            visited[x/2] = visited[x] + 1; 
+            q.push(x/2); 
+        }
+        if (x%3==0 && visited[x/3] == -1) {
+            visited[x/3] = visited[x] + 1; 
+            q.push(x/3); 
+        }
     }
-
+    cout<<visited[1]; 
     return 0;
 }
