813fc787d0eae3b6410df253abcf1b5b1e2ff709
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2026-01-05 14:08:40 +0900

    Title: 두 수의 합, Time: 1095ms, Memory: 3MB, Status: Time Limit Exceed - Codetree

--- 260105/두 수의 합/sum-of-two-num.cpp
+++ 260105/두 수의 합/sum-of-two-num.cpp
@@ -5,23 +5,28 @@ unordered_map<int, int> mp;
 vector<int> v; 
 int main() {
     // Please write your code here.
-    int n, k; 
+
     cin>>n>>k; 
     for(int i=0; i<n; i++) {
         int a; 
         cin>>a; 
         mp[a]++; 
-        v.push_back(a); 
+        if (find(v.begin(), v.end(), a) == v.end()) v.push_back(a); 
     }
     sort(v.begin(), v.end()); 
     int ans=0; 
     for(int i=0; i<n; i++) {
-        if (v[i] > k/2) break; 
-        int a = k-v[i]; 
-        ans = ans + mp[v[i]] * mp[a];
-        // cout<<mp[v[i]]<<":"<<mp[a]<<'\n';
-        
-    }
+        int a=k-v[i]; 
+        if (v[i] > a) break; 
+        if (v[i] != a) {
+            ans += mp[a] * mp[v[i]]; 
+        }
+        else {
+            int b = mp[a]; 
+            ans += (b * (b-1)) / 2; 
+            break; 
+        }
+    }    
     cout<<ans; 
     return 0;
 }
