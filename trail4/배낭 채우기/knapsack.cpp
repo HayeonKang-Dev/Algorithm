e55176e331385b4f02688b404fc25b99ab4309d7
HayeonKang-Dev <163366588+HayeonKang-Dev@users.noreply.github.com>
2026-05-09 18:23:27 +0900

    [Passed] 배낭 채우기 (9ms, 0MB)

---
+++ trail4/배낭 채우기/knapsack.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, M; 
+vector<pair<int, int>> v; 
+int dp[10006]; 
+int main() {
+    // Please write your code here.
+    cin>>N>>M; 
+    for(int i=0; i<N; i++) {
+        int a,b; 
+        cin>>a>>b; 
+        v.push_back({a, b}); 
+    }
+    for(int i=0; i<N; i++) {
+        for(int j=M; j>=v[i].first; j--) {
+            dp[j] = max(dp[j], dp[j-v[i].first] + v[i].second); 
+        }
+    }
+    cout<<dp[M]; 
+    return 0;
+}
